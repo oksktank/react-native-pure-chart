6cfbdaf1cabe26576ccda6cc71a66818a30ac266
Connor O'Rourke <connororourke101@gmail.com>
2020-03-27 12:38:07 +0000

    commented in hitSlop and changed vals to 50

--- examples/pure-chart/components/line-chart.js
+++ examples/pure-chart/components/line-chart.js
@@ -313,7 +313,7 @@ class LineChart extends React.Component {
           });
           this.props.onLongPress(selectedData);
         }}
-        // hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+        hitSlop={{ top: 50, bottom: 50, left: 50, right: 50 }}
       >
         <View
           style={StyleSheet.flatten([
