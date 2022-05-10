4b777120d2528a41343f12798827e033db044022
prabhu <prabhu@prabhus-MacBook-Pro.local>
2022-05-10 16:31:44 +0530

    close tooltip on second click on item

--- examples/pure-chart/components/column-chart.js
+++ examples/pure-chart/components/column-chart.js
@@ -1,70 +1,113 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { View, StyleSheet, Animated, ScrollView, Easing, Text } from 'react-native'
-import ColumnChartItem from './column-chart-item'
-import {initData, drawYAxis, drawYAxisLabels, drawGuideLine, numberWithCommas, drawXAxis, drawXAxisLabels} from '../common'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import {
+  View,
+  StyleSheet,
+  Animated,
+  ScrollView,
+  Easing,
+  Text,
+} from "react-native";
+import ColumnChartItem from "./column-chart-item";
+import {
+  initData,
+  drawYAxis,
+  drawYAxisLabels,
+  drawGuideLine,
+  numberWithCommas,
+  drawXAxis,
+  drawXAxisLabels,
+} from "../common";
 
 export default class ColumnChart extends Component {
-  constructor (props) {
-    super(props)
-    let defaultGap = this.props.defaultColumnWidth + this.props.defaultColumnMargin
-    let newState = initData(this.props.data, this.props.height, defaultGap, this.props.numberOfYAxisGuideLine)
+  constructor(props) {
+    super(props);
+    let defaultGap =
+      this.props.defaultColumnWidth + this.props.defaultColumnMargin;
+    let newState = initData(
+      this.props.data,
+      this.props.height,
+      defaultGap,
+      this.props.numberOfYAxisGuideLine
+    );
     this.state = {
       sortedData: newState.sortedData,
       max: newState.max,
       selectedIndex: null,
       fadeAnim: new Animated.Value(0),
       guideArray: newState.guideArray,
-      gap: defaultGap
-    }
+      gap: defaultGap,
+    };
 
-    this.scrollView = null
+    this.scrollView = null;
 
-    this.renderColumns = this.renderColumns.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-    this.drawTooltip = this.drawTooltip.bind(this)
+    this.renderColumns = this.renderColumns.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.drawTooltip = this.drawTooltip.bind(this);
   }
 
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
     if (nextProps.data !== this.props.data) {
-      this.setState(Object.assign({
-        fadeAnim: new Animated.Value(0)
-      }, initData(nextProps.data, this.props.height, this.state.gap, this.props.numberOfYAxisGuideLine)), () => {
-        Animated.timing(this.state.fadeAnim, { toValue: 1, easing: Easing.bounce, duration: 1000, useNativeDriver: true }).start()
-      })
+      this.setState(
+        Object.assign(
+          {
+            fadeAnim: new Animated.Value(0),
+          },
+          initData(
+            nextProps.data,
+            this.props.height,
+            this.state.gap,
+            this.props.numberOfYAxisGuideLine
+          )
+        ),
+        () => {
+          Animated.timing(this.state.fadeAnim, {
+            toValue: 1,
+            easing: Easing.bounce,
+            duration: 1000,
+            useNativeDriver: true,
+          }).start();
+        }
+      );
     }
   }
 
-  componentDidUpdate(nextProps, nextState){
-    if(this.scrollView != null && nextState.max == 0){
+  componentDidUpdate(nextProps, nextState) {
+    if (this.scrollView != null && nextState.max == 0) {
       setTimeout(
-        () => this.scrollView.scrollTo(this.props.initialScrollPosition), this.props.initialScrollTimeOut
-      )
+        () => this.scrollView.scrollTo(this.props.initialScrollPosition),
+        this.props.initialScrollTimeOut
+      );
     }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     Animated.timing(this.state.fadeAnim, {
-      toValue: 1, easing: Easing.bounce, duration: 1000, useNativeDriver: true
-    }).start()
-    if(this.scrollView != null){
+      toValue: 1,
+      easing: Easing.bounce,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+    if (this.scrollView != null) {
       setTimeout(
-        () => this.scrollView.scrollTo(this.props.initialScrollPosition), this.props.initialScrollTimeOut
-      )
+        () => this.scrollView.scrollTo(this.props.initialScrollPosition),
+        this.props.initialScrollTimeOut
+      );
     }
-    
   }
 
-  renderColumns (fadeAnim) {
-    let seriesArray = this.state.sortedData
-    let seriesCount = seriesArray.length
-    let renderColumns = []
+  renderColumns(fadeAnim) {
+    let seriesArray = this.state.sortedData;
+    let seriesCount = seriesArray.length;
+    let renderColumns = [];
     if (seriesCount > 0) {
-      let standardSeries = seriesArray[0]
-      let dataCount = standardSeries.data.length
+      let standardSeries = seriesArray[0];
+      let dataCount = standardSeries.data.length;
       for (let i = 0; i < dataCount; i++) {
         renderColumns.push(
-          <ColumnChartItem key={i} seriesArray={this.state.sortedData}
+          <ColumnChartItem
+            key={i}
+            seriesArray={this.state.sortedData}
             dataIndex={i}
             defaultWidth={this.props.defaultColumnWidth}
             defaultHeight={this.props.height + 20}
@@ -72,170 +115,243 @@ export default class ColumnChart extends Component {
             defaultBorderColor={this.props.defaultBorderColor}
             isSelected={this.state.selectedIndex === i}
             highlightColor={this.props.highlightColor}
-            onClick={(evt) => this.handleClick(evt, i)} />
-        )
+            onClick={(evt) => this.handleClick(evt, i)}
+          />
+        );
       }
     }
     return (
-      <Animated.View style={[styles.chartView, {
-        transform: [{scaleY: fadeAnim}],
-        marginBottom: this.props.minValue && this.state.guideArray && this.state.guideArray.length > 0 ? -1 * this.state.guideArray[0][2] * this.props.minValue : null
-      }]}>
+      <Animated.View
+        style={[
+          styles.chartView,
+          {
+            transform: [{ scaleY: fadeAnim }],
+            marginBottom:
+              this.props.minValue &&
+              this.state.guideArray &&
+              this.state.guideArray.length > 0
+                ? -1 * this.state.guideArray[0][2] * this.props.minValue
+                : null,
+          },
+        ]}
+      >
         {renderColumns}
       </Animated.View>
-    )
+    );
   }
 
-  handleClick (event, index) {
-    this.setState({
-      selectedIndex: index
-    }, () => {
-      if (typeof this.props.onPress === 'function') {
-        this.props.onPress(index)
-      }
-    })
+  handleClick(event, index) {
+    if (
+      this.state.selectedIndex === null ||
+      this.state.selectedIndex != index
+    ) {
+      this.setState(
+        {
+          selectedIndex: index,
+        },
+        () => {
+          if (typeof this.props.onPress === "function") {
+            this.props.onPress(index);
+          }
+        }
+      );
+    } else {
+      this.setState(
+        {
+          selectedIndex: null,
+        },
+        () => {
+          if (typeof this.props.onPress === "function") {
+            this.props.onPress(null);
+          }
+        }
+      );
+    }
   }
-  drawTooltip (selectedIndex) {
-    if (typeof (selectedIndex) === 'number' && selectedIndex >= 0) {
-      let standardSeries = this.state.sortedData[0]
+  drawTooltip(selectedIndex) {
+    if (typeof selectedIndex === "number" && selectedIndex >= 0) {
+      let standardSeries = this.state.sortedData[0];
       if (!standardSeries) {
-        console.warn('standardSeries is null')
-        return null
+        console.warn("standardSeries is null");
+        return null;
       }
 
-      let seriesCount = this.state.sortedData.length
-      let plusGap = 10 * seriesCount
+      let seriesCount = this.state.sortedData.length;
+      let plusGap = 10 * seriesCount;
       if (this.state.sortedData.length === 1) {
-        plusGap = 0
+        plusGap = 0;
       } else if (selectedIndex === standardSeries.data.length - 1) {
-        plusGap = -50
+        plusGap = -50;
       }
       // 차트 width를 마지막에 늘려야겠음.
 
-      let left = standardSeries.data[selectedIndex]['gap'] + plusGap
-      let tooltipRenders = []
+      let left = standardSeries.data[selectedIndex]["gap"] + plusGap;
+      let tooltipRenders = [];
       for (let i = 0; i < this.state.sortedData.length; i++) {
-        let series = this.state.sortedData[i]
-        if (series.data[selectedIndex]['x']) {
-          tooltipRenders.push(<Text key={'tooltipTitle-' + i} style={styles.tooltipTitle}>{series.data[selectedIndex]['x']}</Text>)
+        let series = this.state.sortedData[i];
+        if (series.data[selectedIndex]["x"]) {
+          tooltipRenders.push(
+            <Text key={"tooltipTitle-" + i} style={styles.tooltipTitle}>
+              {series.data[selectedIndex]["x"]}
+            </Text>
+          );
         }
         tooltipRenders.push(
-          <View key={'tooltipText-' + i} style={{flexDirection: 'row', paddingLeft: 5, alignItems: 'center'}}>
-            <View style={[styles.tooltipColor, {backgroundColor: !series.seriesColor ? this.props.primaryColor : series.seriesColor}]} />
-            <Text style={styles.tooltipValue}>{numberWithCommas(series.data[selectedIndex]['y'], false)}</Text>
+          <View
+            key={"tooltipText-" + i}
+            style={{
+              flexDirection: "row",
+              paddingLeft: 5,
+              alignItems: "center",
+            }}
+          >
+            <View
+              style={[
+                styles.tooltipColor,
+                {
+                  backgroundColor: !series.seriesColor
+                    ? this.props.primaryColor
+                    : series.seriesColor,
+                },
+              ]}
+            />
+            <Text style={styles.tooltipValue}>
+              {numberWithCommas(series.data[selectedIndex]["y"], false)}
+            </Text>
           </View>
-        )
+        );
       }
       return (
         <View style={[styles.tooltipWrapper, { left: left }]}>
-          <View style={styles.tooltip}>
-            {tooltipRenders}
-          </View>
+          <View style={styles.tooltip}>{tooltipRenders}</View>
         </View>
-      )
+      );
     } else {
-      return null
+      return null;
     }
   }
 
-  render () {
-    let {fadeAnim} = this.state
-    if (this.state.sortedData && this.state.sortedData.length === 0) return null
+  render() {
+    let { fadeAnim } = this.state;
+    if (this.state.sortedData && this.state.sortedData.length === 0)
+      return null;
 
     return (
-      <View style={StyleSheet.flatten([styles.wrapper, {
-        backgroundColor: this.props.backgroundColor
-      }])}>
-        <View style={{paddingRight: 5}}>
+      <View
+        style={StyleSheet.flatten([
+          styles.wrapper,
+          {
+            backgroundColor: this.props.backgroundColor,
+          },
+        ])}
+      >
+        <View style={{ paddingRight: 5 }}>
           {this.props.showYAxisLabel &&
-            drawYAxisLabels(this.state.guideArray, this.props.height + 20, this.props.minValue, this.props.labelColor, this.props.yAxisSymbol)}
+            drawYAxisLabels(
+              this.state.guideArray,
+              this.props.height + 20,
+              this.props.minValue,
+              this.props.labelColor,
+              this.props.yAxisSymbol
+            )}
         </View>
         <View style={styles.mainContainer}>
-          <ScrollView ref={scrollView => this.scrollView = scrollView} horizontal>
+          <ScrollView
+            ref={(scrollView) => (this.scrollView = scrollView)}
+            horizontal
+          >
             <View>
-              <View ref='chartView' style={styles.chartContainer}>
+              <View ref="chartView" style={styles.chartContainer}>
                 {drawYAxis(this.props.yAxisColor)}
-                {drawGuideLine(this.state.guideArray, this.props.yAxisGridLineColor)}
+                {drawGuideLine(
+                  this.state.guideArray,
+                  this.props.yAxisGridLineColor
+                )}
                 {this.renderColumns(fadeAnim)}
               </View>
               {drawXAxis(this.props.xAxisColor)}
               <View style={{ marginLeft: this.props.defaultColumnWidth / 2 }}>
                 {this.props.showXAxisLabel &&
-                  drawXAxisLabels(this.state.sortedData[0].data, this.state.gap, this.props.labelColor, this.props.showEvenNumberXaxisLabel)}
+                  drawXAxisLabels(
+                    this.state.sortedData[0].data,
+                    this.state.gap,
+                    this.props.labelColor,
+                    this.props.showEvenNumberXaxisLabel
+                  )}
               </View>
             </View>
             {this.drawTooltip(this.state.selectedIndex)}
           </ScrollView>
         </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    flexDirection: 'row',
-    overflow: 'hidden'
+    flexDirection: "row",
+    overflow: "hidden",
   },
   mainContainer: {
     paddingBottom: 0,
     paddingLeft: 0,
     paddingRight: 0,
-    height: '100%'
+    height: "100%",
   },
   chartContainer: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
+    flexDirection: "row",
+    alignItems: "flex-end",
     margin: 0,
     paddingRight: 10,
-    overflow: 'hidden'
+    overflow: "hidden",
   },
   chartView: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    height: '100%'
+    flexDirection: "row",
+    alignItems: "flex-end",
+    height: "100%",
   },
   tooltipWrapper: {
-    position: 'absolute',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center'
+    position: "absolute",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   tooltip: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     borderRadius: 5,
-    borderColor: '#AAAAAA',
+    borderColor: "#AAAAAA",
     borderWidth: 1,
     padding: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    opacity: 0.8
+    alignItems: "center",
+    justifyContent: "center",
+    opacity: 0.8,
   },
-  tooltipTitle: {fontSize: 10},
-  tooltipValue: {fontWeight: 'bold', fontSize: 15},
+  tooltipTitle: { fontSize: 10 },
+  tooltipValue: { fontWeight: "bold", fontSize: 15 },
   tooltipColor: {
     width: 10,
     height: 5,
     marginRight: 3,
-    borderRadius: 2
-  }
-})
+    borderRadius: 2,
+  },
+});
 
 ColumnChart.propTypes = {
-  data: PropTypes.array
-}
+  data: PropTypes.array,
+};
 ColumnChart.defaultProps = {
   data: [],
   height: 100,
   defaultColumnWidth: 40,
   defaultColumnMargin: 20,
-  defaultBorderColor: '#FFFFFF',
-  primaryColor: '#297AB1',
-  highlightColor: 'red',
+  defaultBorderColor: "#FFFFFF",
+  primaryColor: "#297AB1",
+  highlightColor: "red",
   showEvenNumberXaxisLabel: true,
-  initialScrollPosition: {x: 0, y: 0, animated: true},
+  initialScrollPosition: { x: 0, y: 0, animated: true },
   initialScrollTimeOut: 300,
   showYAxisLabel: true,
   showXAxisLabel: true,
-  yAxisSymbol: ''
-}
+  yAxisSymbol: "",
+};
